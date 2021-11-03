8a3398f2d8f1fab9e6c054c1c43e86271c659112
Muhammad Mazhar <37449942+mazhardev@users.noreply.github.com>
2021-11-04 01:35:33 +0500

    login screen design 50 percent

--- app/screens/LoginScreen.jsx
+++ app/screens/LoginScreen.jsx
@@ -1,11 +1,46 @@
- import React from 'react'
- import { View, Text } from 'react-native'
- 
- export default function LoginScreen() {
-     return (
-         <View>
-             <Text>test</Text>
-         </View>
-     )
- }
- 
+import React from "react";
+import { View, Text, ImageBackground, StyleSheet, Image } from "react-native";
+
+const LoginScreen = () => {
+  return (
+    <ImageBackground
+      source={require("../assets/background.jpg")}
+      style={styles.background}
+    >
+      <View style={styles.logoContainer}>
+        <Image source={require("../assets/logo-red.png")} style={styles.logo} />
+        <Text>Sell What You Don't Need</Text>
+      </View>
+      <View style={styles.loginButton} />
+      <View style={styles.registerButton} />
+    </ImageBackground>
+  );
+};
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  logo: {
+    height: 100,
+    width: 100,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems:'center'
+  },
+  loginButton: {
+    backgroundColor: "red",
+    width: "100%",
+    height: 70,
+  },
+  registerButton: {
+    backgroundColor: "green",
+    width: "100%",
+    height: 70,
+  },
+});
+
+export default LoginScreen;
